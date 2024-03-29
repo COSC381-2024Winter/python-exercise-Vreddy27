a58f2c949acc80481ec562df79224c7d9b03bccb
Vidisha Reddy pallamreddy <vpallamr@emich.edu>
2024-03-29 02:42:02 +0000

    enhancement: creating a list for movie names
    1. added an option in the menu to list all the movie names
    2. also modified the movies.py
    
    issue #3

--- query.py
+++ query.py
@@ -1,8 +1,19 @@
+from movies import Movies
+
 def show_menu():
     print("Menu:")
+    print("Enter list to list all movies")
     print("Enter q to Quit")
 
+def list_all_movies(movies):
+    print("Movie list:")
+    movie_names = movies.list_movie_names()
+    for idx, movie_name in enumerate(movie_names, start=1):
+        print(f"{idx}. {movie_name}")
+
 def main():
+    movies = Movies('./movies.txt') 
+
     while True:
         show_menu()
         choice = input("Choose from the following menu options: ").strip().lower()
@@ -10,6 +21,8 @@ def main():
         if choice == 'q':
             print("Exiting program.")
             break
+        elif choice == 'list':
+            list_all_movies(movies)
         else:
             print("Invalid option. Please try again.")
 
